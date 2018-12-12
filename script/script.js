60f3590f5d5c884bab7b94d55aae27740761d74a
lJOKERl <myweb.joker@gmail.com>
2018-12-12 18:16:13 +0300

    adding control functions

--- script/script.js
+++ script/script.js
@@ -6,11 +6,13 @@ new Vue({
     newGame: false,
     player: {
       name: "Sub Zero",
-      health: 0
+      health: 0,
+      froze: true,
     },
     computer: {
       name: "Ermak",
-      health: 0
+      health: 0,
+      block: false,
     }
   },
   methods: {
@@ -28,25 +30,65 @@ new Vue({
       this.player.name = "Looser";
     },
     attack() {
+      this.player.froze = false;
+      this.computer.block = true;
 
+
+      setTimeout(() => {
+        this.player.froze = true;
+        
+      }, 750);
+
+      
       this.computer.health -= this.calculateDamage(3, 10);
 
       if (this.checkWin()) {
         return;
       }
 
+      setTimeout(() => {
+        this.computer.block = false;
+      }, 1200);
+
+
       this.player.health -= this.calculateDamage(5, 12);
 
       this.checkWin();
-
-      
-
     },
     superAttack() {
+      this.player.froze = false;
+      this.computer.block = true;
 
+
+      setTimeout(() => {
+        this.player.froze = true;
+
+      }, 750);
+
+
+      this.computer.health -= this.calculateDamage(10, 20);
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      setTimeout(() => {
+        this.computer.block = false;
+      }, 1200);
+
+
+      this.player.health -= this.calculateDamage(5, 12);
+
+      this.checkWin();
     },
     heal() {
+      this.player.health += 15;
+      
+      if (this.player.health > 100) {
+        this.player.health = 100;
+      }
 
+      
     },
     calculateDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -71,8 +113,6 @@ new Vue({
       }
 
       return false;
-
     }
-
   }
 });
